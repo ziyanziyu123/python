7a8f2506030ff62a0e49c0475f0b79380d918416
lcy <2664861614@qq.com>
2026-01-29 18:32:17 +0800

    面向对象

---
+++ py_project01/utils/tools.py
@@ -0,0 +1,6 @@
+
+def calc_circle_area(radius):
+    """计算圆的面积"""
+    pi = 3.1415926
+    area = pi * radius ** 2
+    return area
--- py_project01/第二章/函数进阶.py
+++ py_project01/第二章/函数进阶.py
@@ -1,6 +1,17 @@
+import utils.tools
+
 def  main(**kwargs):
     print(kwargs.get('a'))
     if kwargs.get('a'):
         print('a')
 
 main()
+
+area = utils.tools.calc_circle_area(5)
+print(area)
+
+# if __name__ == "__main__":
+
+
+
+    
